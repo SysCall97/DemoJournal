e57a5679b5163f9e8bbba6b8a142c9d0086e1399
Kazi Mashry <kazimashry@gmail.com>
2024-03-13 19:13:12 +0600

    Initial Commit

---
+++ DemoJournal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DemoJournal
+//
+//  Created by Kazi Mashry on 13/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
